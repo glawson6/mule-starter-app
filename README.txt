b052b81497dc06179372b745b21a02d731fc6430
Greg Lawson <glawson6@yahoo.com>
2015-04-21 23:02:12 -0400

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,56 @@
+
+WELCOME
+=======
+
+Congratulations you have just created a new Mule project!
+
+This wizard created a number of new classes and resources useful for new Mule
+projects.  Each of the created files contains documentation and _todo_ items
+where necessary.  Here is an overview of what was created.
+
+./pom.xml:
+
+A maven project descriptor that describes how to build this project.
+
+.src/main/app/mule-config.xml
+
+A basic configuration to get you started.
+
+.src/main/app/mule-deploy.properties
+
+A basic deployment descriptor that controls how the application should be deployed.
+Here you can specify things like the encoding to use, or which domain the app belongs to.
+For more information see: http://www.mulesoft.org/documentation/display/current/Deployment+Descriptor
+
+TESTING
+=======
+
+This project also contains test classes that can be run as part of a test suite.
+
+-----------------------------------------------------------------
+./src/test/java/org/taptech/app/ExampleFunctionalTestCase.java
+
+This is an example functional test case.  The test will work as is, but you
+need to configure it to actually test your code.
+
+For more information about
+testing see: http://www.mulesoft.org/documentation/display/current/Functional+Testing.
+
+ADDITIONAL RESOURCES
+====================
+
+Everything you need to know about getting started with Mule can be found here:
+http://www.mulesoft.org/documentation/display/current/Home
+
+For information about working with Mule Studio which is the Mule IDE can be found here:
+http://www.mulesoft.org/documentation/display/current/Anypoint+Studio+Essentials
+
+Remember if you get stuck you can try getting help on the Mule forum:
+http://forum.mulesoft.org/mulesoft
+
+Also, MuleSoft, the company behind Mule, offers 24x7 support options:
+http://www.mulesoft.com/support-and-services/enterprise-subscriptions-support
+
+Enjoy your Mule ride!
+
+The Mule Team
---
+++ src/main/java/org/taptech/app/MuleBootstrap.java
@@ -0,0 +1,73 @@
+package org.taptech.app;
+
+
+import org.mule.api.MuleContext;
+import org.mule.api.MuleException;
+import org.mule.api.config.ConfigurationException;
+import org.mule.api.lifecycle.InitialisationException;
+import org.mule.config.spring.SpringXmlConfigurationBuilder;
+import org.mule.context.DefaultMuleContextFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.StaticApplicationContext;
+
+/**
+ * Created by tap on 4/18/15.
+ */
+//@SpringBootApplication
+@ComponentScan
+@Configuration
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,DispatcherServletAutoConfiguration.class,
+        WebMvcAutoConfiguration.class, EmbeddedServletContainerAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+public class MuleBootstrap implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(MuleBootstrap.class);
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        DefaultMuleContextFactory muleContextFactory = new DefaultMuleContextFactory();
+        SpringXmlConfigurationBuilder configBuilder = null;
+        try {
+            StaticApplicationContext staticApplicationContext = new StaticApplicationContext(context);
+            configBuilder = new SpringXmlConfigurationBuilder("mule-config.xml");
+            staticApplicationContext.refresh();
+            configBuilder.setParentContext(staticApplicationContext);
+            ConfigurableApplicationContext configurableApplicationContext = (ConfigurableApplicationContext)configBuilder.getApplicationContext();
+            MuleContext muleContext = muleContextFactory.createMuleContext(configBuilder);
+            muleContext.start();
+            log.info("Started Mule!");
+        } catch (ConfigurationException e) {
+            e.printStackTrace();
+        } catch (InitialisationException e) {
+            e.printStackTrace();
+        } catch (MuleException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String... args) {
+        log.info("Starting to run...");
+        SpringApplication app = new SpringApplication(MuleBootstrap.class);
+        app.setWebEnvironment(false);
+        app.run(args);
+        log.info("Completed the run...");
+    }
+
+
+}
